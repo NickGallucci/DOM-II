ed7573d8625703e5b1ecf528aa12a0fe7e328855
CryptoN999 <nicholasgallucci4@gmail.com>
2019-08-09 19:29:36 -0700

    MVP completed

--- js/index.js
+++ js/index.js
@@ -31,13 +31,42 @@ card.addEventListener('dblclick', function (e) {
 
 //5
 
+const lol = document.querySelectorAll('nav a');
+const home = lol[0];
+const aboutUs = lol[1];
+const blog = lol[2];
+const contact = lol[3];
+
+
+aboutUs.addEventListener('focus', e => e.target.style.backgroundColor = 'red');
+
 //6
+home.addEventListener('blur', e => {
+   e.target.style.backgroundColor = 'yellow';
+   e.stopPropagation();
+});
 
 //7
+aboutUs.addEventListener('pointerover', ()=> console.log("MALICIOUS LINK!!!"));
 
 //8
+window.addEventListener('load', ()=> console.log("Look behind you!"))
 
 //9
+const imgContentImg = document.querySelector('.img-content img');
+imgContentImg.addEventListener('wheel', zoom);
+let scale = 1;
+
+function zoom(e){
+	event.preventDefault();
+	scale += event.deltaY * -0.91; 
+	scale = Math.min(Math.max(1, scale), 4); 
+    imgContentImg.style.transform = `scale(${scale})`; 
+    
+};
 
 //10
-
+function logKey(e){
+	console.log(e.code);
+}
+document.addEventListener('keydown', logKey);
